4567d980ab1892ea300e8e6c33947879cb48ffec
abdopcnet <abdopcnet@gmail.com>
2026-01-02 02:47:27 +0000

    02-47-02-01-2026

---
+++ tatweer_custom_fields/tatweer_custom_fields/doctype/tatweer_workspaces_acess/tatweer_workspaces_acess.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, abdopcnet@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Tatweer Workspaces Acess", {
+// 	refresh(frm) {
+
+// 	},
+// });
